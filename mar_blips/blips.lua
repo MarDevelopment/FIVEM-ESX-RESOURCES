5ea2aa4c52a73fb8dfbfab812c1072cfc6fe401f
sorenmarquardt <soren8620@hotmail.com>
2021-03-30 21:55:48 +0200

    Mulighed for at sætte blips

---
+++ mar_blips/blips.lua
@@ -0,0 +1,20 @@
+local blips = {
+    -- Example {title="", colour=, id=, x=, y=, z=},
+
+    {title="Biograf", colour=27, id=629, x = 336.05, y = 175.56, z = 103.18},
+  }
+      
+Citizen.CreateThread(function()
+
+    for _, info in pairs(blips) do
+      info.blip = AddBlipForCoord(info.x, info.y, info.z)
+      SetBlipSprite(info.blip, info.id)
+      SetBlipDisplay(info.blip, 4)
+      SetBlipScale(info.blip, 1.0)
+      SetBlipColour(info.blip, info.colour)
+      SetBlipAsShortRange(info.blip, true)
+	  BeginTextCommandSetBlipName("STRING")
+      AddTextComponentString(info.title)
+      EndTextCommandSetBlipName(info.blip)
+    end
+end)
---
+++ mar_blips/fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'adamant'
+games { 'gta5' }
+description "FiveM Blips"
+
+client_script {
+    "blips.lua"
+}
